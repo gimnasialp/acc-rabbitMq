b0cbeae8011d76161a4aedd32a25670e57de7a78
migue.centellas@gmail.com <migue.centellas@gmail.com>
2024-04-22 23:21:34 -0300

    envio de mas mensajes desde un string

--- src/main/java/com/demo/rabbitmq/Publisher.java
+++ src/main/java/com/demo/rabbitmq/Publisher.java
@@ -14,13 +14,13 @@ public class Publisher {
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 		
-		String message = "First message from RabbitMQ";
+		//String message = "First message from RabbitMQ";
 		
-		//String[] messages = {"First", "Second", "Third", "Fourth"};
+		String[] messages = {"First", "Second", "Third", "Fourth"};
 		
-		//for(String message : messages) {
+		for(String message : messages) {
 			channel.basicPublish("", "Queue-1", null, message.getBytes());
-		//}
+		}
 		
 		channel.close();
 		connection.close();
